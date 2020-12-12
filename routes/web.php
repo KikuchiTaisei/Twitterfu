be10c471e8f3cc836f754c3cdf9b515327354d6b
EC2 Default User <ec2-user@ip-172-31-17-235.ap-northeast-1.compute.internal>
2020-12-12 09:06:08 +0000

    set up bootsrap4

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('posts', 'PostControler');
+
+if (env('APP_ENV') === 'local') {
+    URL::forceScheme('https');
+}
